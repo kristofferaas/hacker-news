0be772b1a04eb56ade5a1d40f877b84c53d07edf
Kristoffer Aas <kristoffer.aas@fortedigital.com>
2023-07-28 12:46:44 +0200

    Adds pagination

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,7 @@
 import { Points } from "@/components/hacker-news/points";
 import { Button } from "@/components/ui/button";
 import { getItem, search, NumericFilter } from "@/lib/api";
+import { qs } from "@/lib/utils";
 import { Arrow } from "@radix-ui/react-select";
 import { formatDistance } from "date-fns";
 import { ArrowLeft, ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
@@ -10,29 +11,34 @@ import { z } from "zod";
 const searchParamsSchema = z.object({
   query: z.string().optional(),
   time: z.enum(["today", "week", "month", "year", "all"]).optional(),
+  page: z.coerce.number().min(0).optional(),
 });
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const { query, time } = searchParamsSchema.parse(searchParams);
+type SearchParams = z.infer<typeof searchParamsSchema>;
 
-  const posts = await search({
-    query: query,
+export default async function Home(props: { searchParams: unknown }) {
+  const searchParams = searchParamsSchema.parse(props.searchParams);
+
+  const { hits, page, nbPages, nbHits, query } = await search({
+    query: searchParams.query,
     tags: ["story"],
-    numericFilters: createNumericFilter({ time }),
+    numericFilters: createNumericFilter(searchParams),
+    page: searchParams.page,
   });
 
   return (
     <main className="container max-w-5xl my-2 space-y-2">
+      {query && (
+        <span className="text-sm text-muted-foreground">
+          {nbHits} results for &quot;{query}&quot;
+        </span>
+      )}
       <ol className="space-y-2">
-        {posts.hits.map((hit) => (
+        {hits.map((hit) => (
           <Item key={hit.objectID} id={hit.objectID} />
         ))}
       </ol>
-      <Pagination />
+      <Pagination page={page} pageCount={nbPages} searchParams={searchParams} />
     </main>
   );
 }
@@ -68,16 +74,47 @@ async function Item({ id }: { id: string }) {
   );
 }
 
-const Pagination = () => {
+const Pagination = ({
+  page,
+  pageCount,
+  searchParams,
+}: {
+  page: number;
+  pageCount: number;
+  searchParams: SearchParams;
+}) => {
+  const pages = Array.from({ length: pageCount }, (_, i) => i);
+
+  const prevPage = Math.max(0, page - 1);
+  const nextPage = Math.min(pageCount - 1, page + 1);
+
   return (
-    <div className="flex space-x-2">
-      <Button variant="outline" size="icon" asChild>
-        <Link href="/posts?page=2">
+    <div className="flex">
+      <Button className="shrink-0" variant="outline" size="icon" asChild>
+        <Link href={`/posts?${qs({ ...searchParams, page: prevPage })}`}>
           <ArrowLeftIcon className="w-4 h-4" />
         </Link>
       </Button>
-      <Button variant="outline" size="icon" asChild>
-        <Link href="/posts?page=2">
+      <div className="flex flex-grow space-x-2 px-2 overflow-auto">
+        {pages.map((p) => (
+          <Button
+            key={p}
+            size="icon"
+            className="flex-shrink-0"
+            variant={p === page ? "default" : "outline"}
+            asChild
+          >
+            <Link
+              href={`/posts?${qs({ ...searchParams, page: p })}`}
+              prefetch={false}
+            >
+              {p}
+            </Link>
+          </Button>
+        ))}
+      </div>
+      <Button className="shrink-0" variant="outline" size="icon" asChild>
+        <Link href={`/posts?${qs({ ...searchParams, page: nextPage })}`}>
           <ArrowRightIcon className="w-4 h-4" />
         </Link>
       </Button>
@@ -85,9 +122,7 @@ const Pagination = () => {
   );
 };
 
-const createNumericFilter = ({
-  time,
-}: z.infer<typeof searchParamsSchema>): NumericFilter => {
+const createNumericFilter = ({ time }: SearchParams): NumericFilter => {
   switch (time) {
     case "all": {
       return ["created_at_i", ">", 0];
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Build a query string from an object
+export const qs = (obj: Record<string, string | number | boolean>) =>
+  Object.keys(obj)
+    .map((key) => `${key}=${obj[key]}`)
+    .join("&");
